d809e76773ae7d5743b078acf689594826f15b1a
Felipe Ferreira <felipehfj@gmail.com>
2020-04-25 16:30:51 -0300

    Update pet

--- frontend/src/views/Pet/index.js
+++ frontend/src/views/Pet/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import { useToasts } from 'react-toast-notifications';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input } from 'reactstrap';
 import './styles.css';
 import NavigationBar from '../../components/NavigationBar';
 import api from '../../services/api';
@@ -9,14 +10,50 @@ export default function Pet() {
   const { id } = useParams();
 
   const [refreshPage, setRefreshpage] = useState(0);
+  const [animals, setAnimals] = useState([]);
   const [name, setName] = useState('');
   const [color, setColor] = useState('');
   const [animal, setAnimal] = useState('');
   const [animalName, setAnimalName] = useState('');
   const { addToast } = useToasts();
 
+  const [showModal, setShowModal] = useState(false);
+  function toggleModal() {
+    setShowModal(!showModal);
+    if (!showModal) {
+      getAnimals();
+    }
+  }
+
   const history = useHistory();
 
+  function updatePet(e) {
+    if (e) e.preventDefault();
+    api.patch(`pets/${id}`, {
+      name, color, animal
+    })
+      .then(response => {
+        setRefreshpage(refreshPage + 1)
+        addToast('Pessoa salva com sucesso', { appearance: 'success' });
+      })
+      .catch(e => {
+        addToast(e.response.data.message, { appearance: 'error' });
+      })
+  }
+
+  function getAnimals(e) {
+    if (e) e.preventDefault();
+    api.get(`animals`, {
+      page: 1,
+      size: 10000
+    })
+      .then(response => {
+        setAnimals(response.data)
+      })
+      .catch(e => {
+        addToast(e.response.data.message, { appearance: 'error' });
+      })
+  }
   useEffect(() => {
     api.get(`pets/${id}`)
       .then(response => {
@@ -32,27 +69,79 @@ export default function Pet() {
   function updatePet(e) {
     if (e) e.preventDefault();
     api.patch(`pets/${id}`, {
-      name, color, animal, animalName
+      name, color, animal
     })
       .then(response => {
         setRefreshpage(refreshPage + 1)
-        addToast('Pessoa salva com sucesso', { appearance: 'success' });
+        addToast('Pet salvo com sucesso', { appearance: 'success' });
+        toggleModal();
       })
       .catch(e => {
         addToast(e.response.data.message, { appearance: 'error' });
       })
-  }  
+  }
   return (
     <div>
       <NavigationBar />
-      <div><div className="card" >
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">Cor: {color}</p>
-          <p className="card-text">Tipo: {animalName}</p>
-          <Link className="btn btn-primary btn-block" to={'/pets'}>Voltar</Link>
-        </div>
-      </div></div>
+      <div>
+        <Card>
+          <CardBody className="card-body">
+            <CardTitle className="card-title">{name}</CardTitle>
+            <CardSubtitle >Cor: {color}</CardSubtitle>
+            <CardText>Tipo: {animalName}</CardText>
+            <Link className="btn btn-primary btn-block" to={'/pets'}>Voltar</Link>
+            <Button onClick={toggleModal}>Alterar</Button>
+          </CardBody>
+        </Card>
+      </div>
+
+      <Modal isOpen={showModal} toggle={toggleModal} className="modal-lg" backdrop={'static'} keyboard={false}>
+        <ModalHeader toggle={toggleModal}>Modal title</ModalHeader>
+        <ModalBody>
+          <div>
+            <label className="control-label">{id}</label>
+
+            <FormGroup>
+              <Label className="control-label" for="nome">Nome</Label>
+              <Input
+                id="nome"
+                className="form-control"
+                placeholder="Nome do Pet"
+                required
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label className="control-label" for="cor">Cor</Label>
+              <Input
+                id="cor"
+                className="form-control"
+                type="text"
+                placeholder="Cor do animal"
+                value={color}
+                onChange={e => setColor(e.target.value)}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label className="control-label" for="animal">Tipo do Pet</Label>
+              <Input type="select" className="form-control" id="animal" value={animal} onChange={e => setAnimal(e.target.value)}>
+                {animals.map(item => (
+                  <option key={item.id} value={item.id}>{item.name}</option>
+                ))}
+              </Input>
+            </FormGroup>
+
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggleModal}>Cancelar</Button>
+          <Button color="primary" onClick={updatePet}>Alterar</Button>
+        </ModalFooter>
+      </Modal>
+
     </div>
   );
 }
